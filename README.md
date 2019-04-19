7d3a8650989503f7f64d04e0e44283b740835e5c
Will <willhy@gmail.com>
2019-04-18 20:42:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Pi Sound Machine
+
+Using the [pHAT BEAT](https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-phat-beat) from [Pimoroni](https://learn.pimoroni.com/) I was able to make a very rudimentary sound machine to play white noise at night while I sleep.
+
+The configuration scripts from Pimoroni should install the essential libraries and configuration files needed for the pHAT BEAT to function.
+
+Add this to `/etc/rc.local` to get the script to run at boot.
+
+```
+sudo -u pi python3 /home/pi/audio.py &
+```
+
+This line in `/etc/asound.conf` disables the lights on the pHAT BEAT.
+
+```
+pcm_scope.pivumeter {
+    ...
+    brightness 0
+    ...
+}
+```
+
+The `play/pause` button works as expected as should the `+` and `-` for changing volume. Modify the code as expected and copy over an `mp3` audio file for the white noise as needed.
---
+++ audio.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import phatbeat
+import signal
+import os
+from subprocess import call, Popen
+
+proc = None
+interrupted = False
+playing = False
+
+FNULL = open(os.devnull, 'w')
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+signal.signal(signal.SIGINT, signal_handler)
+
+@phatbeat.on(phatbeat.BTN_PLAYPAUSE)
+def playpause(pin):
+    global playing
+    global proc
+    if playing:
+        print('stop playing')
+        call(['kill', '-9', '%d' % proc.pid])
+        proc.wait()
+        playing = False
+    else:
+        print('start playing')
+        proc = Popen(['mpg123', '--loop', '-1', '/home/pi/waterfall.mp3'], shell=False, stdout=FNULL, stderr=FNULL)
+        playing = True
+
+@phatbeat.on(phatbeat.BTN_VOLUP)
+def volume_up(pin):
+    call(['amixer', 'set', 'Master', '10%+'], stdout=FNULL, stderr=FNULL)
+
+@phatbeat.on(phatbeat.BTN_VOLDN)
+def volume_down(pin):
+    call(['amixer', 'set', 'Master', '10%-'], stdout=FNULL, stderr=FNULL)
+
+if __name__ == "__main__":
+    print("starting pirate radio script")
+    while True:
+        if interrupted:
+            print("exiting pirate radio script")
+            break
+
